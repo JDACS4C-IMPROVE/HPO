eba628b5b44313be4332f432e747b8f957b9d91f
Andreas Wilke <wilke@anl.gov>
2023-09-18 13:40:08 -0500

    Initial commit

---
+++ tests/GraphDRP/cfg-nightly.sh
@@ -0,0 +1,6 @@
+source_cfg -v /homes/wilke/IMPROVE/HPO/tests/GraphDRP/cfg-graphdrp-hpo-settings.sh
+
+export CANDLE_MODEL_TYPE="SINGULARITY"
+export MODEL_NAME=/software/improve/images//GraphDRP.sif
+export PARAM_SET_FILE=/homes/wilke/IMPROVE/HPO/tests/GraphDRP/hpo-parameter-space.json
+
