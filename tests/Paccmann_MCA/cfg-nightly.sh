f878963fb1f1b6fc268445c967436844d85fb91a
Andreas Wilke <wilke@anl.gov>
2023-09-19 15:53:56 -0500

    Added configs for tests

---
+++ tests/Paccmann_MCA/cfg-nightly.sh
@@ -0,0 +1,6 @@
+source_cfg -v /homes/wilke/IMPROVE/HPO/tests/Paccmann_MCA/cfg-hpo-settings.sh
+
+export CANDLE_MODEL_TYPE="SINGULARITY"
+export MODEL_NAME=/software/improve/images/Paccmann_MCA.sif
+export PARAM_SET_FILE=/homes/wilke/IMPROVE/HPO/tests/Paccmann_MCA/cfg-hpo-parameter-space.json
+
