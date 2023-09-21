e95d12608b0470754f0285993b031d5debab3437
Andreas Wilke <wilke@anl.gov>
2023-09-21 15:16:53 -0500

    Deleted template files; modified cfg-nightly for DRPreter and Paccmann_MCA

--- tests/DRPreter/cfg-nightly.sh
+++ tests/DRPreter/cfg-nightly.sh
@@ -1,6 +1,8 @@
-source_cfg -v ${BASE_DIR}/tests/DRPreter/cfg-hpo-settings.sh
+source_cfg -v ${HPO_BASE_DIR}/tests/DRPreter/cfg-hpo-settings.sh
 
 export CANDLE_MODEL_TYPE="SINGULARITY"
-export MODEL_NAME=${IMAGE_PATH}/DRPreter.sif
-export PARAM_SET_FILE=${BASE_DIR}/tests/DRPreter/cfg-hpo-parameter-space.json
+export MODEL_NAME=${HPO_IMAGE_DIR}/DRPreter.sif
+export PARAM_SET_FILE=${HPO_BASE_DIR}/tests/DRPreter/cfg-hpo-parameter-space.json
 
+export CANDLE_DATA_DIR=${HPO_DATA_DIR}/$(basname $MODEL_NAME .sif)
+mkdir -p ${CANDLE_DATA_DIR}
--- tests/Paccmann_MCA/cfg-nightly.sh
+++ tests/Paccmann_MCA/cfg-nightly.sh
@@ -1,6 +1,11 @@
-source_cfg -v ${BASE_DIR}/tests/Paccmann_MCA/cfg-hpo-settings.sh
 
+export MODEL_NAME=${HPO_IMAGE_DIR}/Paccmann_MCA.sif
+
+MODEL=$$(basname $MODEL_NAME .sif)
+
+source_cfg -v ${HPO_BASE_DIR}/tests/${MODEL}/cfg-hpo-settings.sh
 export CANDLE_MODEL_TYPE="SINGULARITY"
-export MODEL_NAME=${IMAGE_PATH}/Paccmann_MCA.sif
-export PARAM_SET_FILE=${BASE_DIR}tests/Paccmann_MCA/cfg-hpo-parameter-space.json
+export PARAM_SET_FILE=${HPO_BASE_DIR}/tests/${MODEL}/cfg-hpo-parameter-space.json
+export CANDLE_DATA_DIR=${HPO_DATA_DIR}/${MODEL}
 
+mkdir -p ${CANDLE_DATA_DIR}
