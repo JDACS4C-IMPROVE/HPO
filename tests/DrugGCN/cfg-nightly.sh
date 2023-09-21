40469d5701b18799692b1ffcd56fb7eacc34e549
Andreas Wilke <wilke@anl.gov>
2023-09-21 15:24:09 -0500

    Updated DrugGCN

--- tests/DrugGCN/cfg-nightly.sh
+++ tests/DrugGCN/cfg-nightly.sh
@@ -1,6 +1,11 @@
-source_cfg -v ${BASE_DIR}/tests/DrugGCN/cfg-hpo-settings.sh
 
+export MODEL_NAME=${HPO_IMAGE_DIR}/DrugGCN.sif
+
+MODEL=$(basname $MODEL_NAME .sif)
+
+source_cfg -v ${HPO_BASE_DIR}/tests/${MODEL}/cfg-hpo-settings.sh
 export CANDLE_MODEL_TYPE="SINGULARITY"
-export MODEL_NAME=${IMAGE_PATH}/DrugGCN.sif
-export PARAM_SET_FILE=/${BASE_DIR}/tests/DrugGCN/cfg-hpo-parameter-space.json
+export PARAM_SET_FILE=${HPO_BASE_DIR}/tests/${MODEL}/cfg-hpo-parameter-space.json
+export CANDLE_DATA_DIR=${HPO_DATA_DIR}/${MODEL}
 
+mkdir -p ${CANDLE_DATA_DIR}
