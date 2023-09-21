340dfcd7912da96b6de9884aacdb6fba863fde77
Andreas Wilke <wilke@anl.gov>
2023-09-21 15:30:37 -0500

    Update config for DRPreter

--- tests/DRPreter/cfg-nightly.sh
+++ tests/DRPreter/cfg-nightly.sh
@@ -1,8 +1,11 @@
-source_cfg -v ${HPO_BASE_DIR}/tests/DRPreter/cfg-hpo-settings.sh
+
+export MODEL_NAME=${HPO_IMAGE_DIR}/DRPreter.sif
+
+MODEL=$(basname $MODEL_NAME .sif)
 
 export CANDLE_MODEL_TYPE="SINGULARITY"
-export MODEL_NAME=${HPO_IMAGE_DIR}/DRPreter.sif
-export PARAM_SET_FILE=${HPO_BASE_DIR}/tests/DRPreter/cfg-hpo-parameter-space.json
+export PARAM_SET_FILE=${HPO_BASE_DIR}/tests/${MODEL}/cfg-hpo-parameter-space.json
+export CANDLE_DATA_DIR=${HPO_DATA_DIR}/${MODEL}
 
-export CANDLE_DATA_DIR=${HPO_DATA_DIR}/$(basname $MODEL_NAME .sif)
 mkdir -p ${CANDLE_DATA_DIR}
+source_cfg -v ${HPO_BASE_DIR}/tests/${MODEL}/cfg-hpo-settings.sh
