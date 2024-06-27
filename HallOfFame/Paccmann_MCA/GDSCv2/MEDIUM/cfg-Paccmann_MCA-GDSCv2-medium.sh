8d51410cbf4d1c86df7ad0be06b2a4c0993db9ed
Rajeev Jain <rajeeja@gmail.com>
2024-06-27 19:01:14 +0000

    o Add medium runs for GDSCv1 GDSCv2 datasets

---
+++ HallOfFame/Paccmann_MCA/GDSCv2/MEDIUM/cfg-Paccmann_MCA-GDSCv2-medium.sh
@@ -0,0 +1,5 @@
+export MODEL=Paccmann_MCA
+source_cfg -v /lus/eagle/projects/IMPROVE_Aim1/rjain/HPO//experiments/HPO2404/configs/Paccmann_MCA/cfg-medium-settings.sh
+export CANDLE_MODEL_TYPE=SINGULARITY
+export MODEL_NAME=/lus/eagle/projects/IMPROVE_Aim1/images//Paccmann_MCA.sif
+export PARAM_SET_FILE=/lus/eagle/projects/IMPROVE_Aim1/rjain/HPO//experiments/HPO2404/configs/Paccmann_MCA/hyperparams.Paccmann_MCA.GDSCv2.medium.json
